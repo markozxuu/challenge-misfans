acc6e085d52bdee6246be8d5c05eafc55407d464
Markoz Pena <markdrew53@gmail.com>
2022-03-12 03:40:15 -0600

    Create search svg icon

---
+++ components/icons/Search.tsx
@@ -0,0 +1,17 @@
+const Search = () => (
+  <svg
+    className="absolute w-5 h-5 text-gray-400 right-3 top-3"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </svg>
+);
+
+export default Search;
