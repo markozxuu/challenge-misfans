d72052eb1c1e32978bdd1d01e3ce325f37be9dbf
Markoz Pena <markdrew53@gmail.com>
2022-03-12 03:40:49 -0600

    Create logo svg icon

---
+++ components/icons/Logo.tsx
@@ -0,0 +1,17 @@
+const Logo = () => (
+  <span className="font-extrabold">
+    socialred.verce.app
+    <style jsx>{`
+      span {
+        font-size: 18px;
+        display: inline-flex;
+        align-items: center;
+        height: 30px;
+        line-height: 20px;
+        padding: 10px;
+      }
+    `}</style>
+  </span>
+);
+
+export default Logo;
