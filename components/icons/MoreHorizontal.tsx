6139d3bee0f8d0eff5408e48308e1fb8c091ab82
Markoz Pena <markdrew53@gmail.com>
2022-04-11 15:47:23 -0500

    Add `moreHorizontal` icon

---
+++ components/icons/MoreHorizontal.tsx
@@ -0,0 +1,19 @@
+const MoreHorizontal = () => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    shapeRendering="geometricPrecision"
+  >
+    <circle cx="12" cy="12" r="1" fill="currentColor" />
+    <circle cx="19" cy="12" r="1" fill="currentColor" />
+    <circle cx="5" cy="12" r="1" fill="currentColor" />
+  </svg>
+);
+
+export default MoreHorizontal;
