9bb7ba1cb1e3cdca4319702bab6b64e4b97b77f8
Markoz Pena <markdrew53@gmail.com>
2022-04-11 15:50:15 -0500

    Add a new box shadow call me `popover`

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       boxShadow: {
         magical: '0 0 15px 0 rgb(0 0 0 / 10%)',
         'magical-dark': ' 0 0 0 1px #333',
+        popover: '0 8px 30px rgba(0,0,0,0.12)',
       },
     },
   },
