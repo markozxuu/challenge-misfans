737d1fc8104ab53946e497304a14ab9bfb4fc3f6
Markoz Pena <markdrew53@gmail.com>
2022-04-11 15:56:22 -0500

    Add the button the authentication

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import throttle from 'lodash.throttle';
 import { useState, useEffect } from 'react';
+import { useSession, signIn } from 'next-auth/react';
 
 import Logo from '@components/icons/Logo';
 import cx from 'clsx';
@@ -8,10 +9,21 @@ import cx from 'clsx';
 import s from './navbar.module.css';
 import SwitchTheme from '@components/ui/Switch';
 
+import { createUser } from '@utils/api';
+import PopoverUser from '@components/ui/PopoverUser';
+import { KEY_CAHE } from '@utils/const';
+
 const Navbar = () => {
+  const { data: session, status } = useSession();
   const [isScrolled, setScrolled] = useState(false);
 
   useEffect(() => {
+    if (!localStorage.getItem(KEY_CAHE)) {
+      if (session) {
+        createUser(session?.user);
+      }
+    }
+
     const handleScrolled = throttle(() => {
       const offset = 0;
       const { scrollTop } = document.documentElement;
@@ -24,7 +36,8 @@ const Navbar = () => {
     return () => {
       document.removeEventListener('scroll', handleScrolled);
     };
-  }, [isScrolled]);
+  }, [isScrolled, session]);
+  if (status === 'loading') return null;
   return (
     <nav
       className={cx(s.root, 'bg-white dark:bg-[#131415]', {
@@ -40,14 +53,18 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center space-x-5 mt-5 md:mt-0">
           <SwitchTheme />
-          <a
-            href="https://github.com/markozxuu/challenge-misfans"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-black dark:bg-white py-1 px-4 rounded-md text-white dark:text-black text-center"
-          >
-            Source
-          </a>
+          {session ? (
+            <div>
+              <PopoverUser profileImage={session?.user?.image} />
+            </div>
+          ) : (
+            <button
+              onClick={() => signIn('github')}
+              className="bg-black dark:bg-white py-1 px-4 rounded-md text-white dark:text-black text-center"
+            >
+              Sign In
+            </button>
+          )}
         </div>
       </div>
     </nav>
