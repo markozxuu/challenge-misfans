64069a214c453450ded45832da29a5912682e83c
Markoz Pena <markdrew53@gmail.com>
2022-04-11 15:51:50 -0500

    Add `KEY_CACHE`

--- utils/const.ts
+++ utils/const.ts
@@ -1,2 +1,5 @@
 export const API = 'https://dummyapi.io/data/v1/user?page=1&limit=10';
 export const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+export const GITHUB_ID = process.env.GITHUB_ID;
+export const GTIHUB_SECRET = process.env.GITHUB_SECRET;
+export const KEY_CAHE = 'userIdDummy.io';
